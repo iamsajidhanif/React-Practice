215fdb203a1f2a0db091138f81d0099e89351494
sajidhanif555 <sajidhanif555@gmail.com>
2023-03-12 19:24:39 +0500

    index.html, App.js and App.css updated after practicing with CWH

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+let name = 'Sajid Hanif';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello World
+          </a>
+        </header>
+      </div> */}
+
+      <class className="blank">Lovely</class>
+
+      <nav className='mynav'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+
+      <h1 className="heading1">Paragraph No. 1</h1>
+
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, error vero ullam, consectetur et reiciendis deserunt quaerat vitae sequi enim laudantium odit numquam ad minima eum pariatur, possimus suscipit distinctio. Commodi vitae dolore consectetur vero? Ab at eveniet, fuga iusto accusantium reprehenderit ipsum nobis excepturi atque, dignissimos esse eos ratione!</p>
+      </div>
+
+      <h1>Hello, {name}</h1>
+    </>
   );
 }
 
